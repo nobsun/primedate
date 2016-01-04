4db1cb8b94e74a300c1b2a5fb318724cea59f2df
Nobuo Yamashita <nobsun@sampou.org>
2016-01-04 15:28:18 +0900

    release primedate

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,17 @@
 module Main where
 
-import Lib
+import Data.Char
+import Data.Time.Calendar
+import System.Environment
+import Math.NumberTheory.Primes
 
 main :: IO ()
-main = someFunc
+main = putStr . unlines . map showGregorian . primeDates . read . head =<< getArgs
+
+primeDates :: Integer -> [Day]
+primeDates n = filter isPrimeDate [jan1 .. addDays yds jan1]
+  where yds = if isLeapYear n then 365 else 364
+        jan1 = fromGregorian n 1 1
+
+isPrimeDate :: Day -> Bool
+isPrimeDate =  isPrime . read . filter isDigit . showGregorian
